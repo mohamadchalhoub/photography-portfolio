7026b718d63032735dfad8430bd10d6fe108073e
mohamadchalhoub <108233015+mohamadchalhoub@users.noreply.github.com>
2025-08-20 16:03:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Abdul kader Al Bay App',
+  description: 'Created by Mohamad Chalhoub',
+  generator: '',
 }
 
 export default function RootLayout({
